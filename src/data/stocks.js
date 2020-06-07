4facb772472329e653c55a70aa05ea39bd080642
ahmedfarghaly <ahmedfarghaly505@mu.edu.sa>
2020-06-07 09:09:18 +0300

    finish trader project

---
+++ src/data/stocks.js
@@ -0,0 +1,9 @@
+export default [
+    {id:'1', name:'BMW', price:'100'},
+    {id:'2', name:'JEEB', price:'200'},
+    {id:'3', name:'APPLLE', price:'300'},
+    {id:'4', name:'VERNA', price:'250'},
+    {id:'5', name:'SAMSUNG', price:'350'},
+    {id:'6', name:'AMAZON', price:'450'}
+]
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import vueResource from 'vue-resource';
+import {routes} from './routes';
+import store from './store/store';
 
+Vue.use(VueRouter);
+Vue.use(vueResource);
+Vue.http.options.root = 'https://vue-stock-trader-ad55e.firebaseio.com/';
 Vue.config.productionTip = false
-
+const router = new VueRouter({
+  mode:'history',
+  routes,
+})
+Vue.filter('currency', (value) => {
+  return '$' + value.toLocaleString();
+});
 new Vue({
   render: h => h(App),
+  router,
+  store,
 }).$mount('#app')
